938d6ab870538857a52d2ae2489f717fd5eff22d
Utkarshsisodia <usisodia67@gmail.com>
2025-06-01 11:50:56 +0530

    Add express-session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import session from "express-session";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,14 @@ const saltRound = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: "TOPSECRETEWORD",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const db = new pg.Client({
   user: "postgres",
@@ -41,7 +50,6 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("email already exists. Try logging in.");
     } else {
-      
       //Password Hashing
       bcrypt.hash(password, saltRound, async (err, hash) => {
         if (err) {
